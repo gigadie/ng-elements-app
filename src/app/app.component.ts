5da067c3b186494237fbfcf61d50b5e81b6b4bfe
Diego Fernandez <diefer82@gmail.com>
2019-07-15 17:11:41 +0100

    feat: custom-element with communication with host

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-elements-app';
+
+  @Input('state') 
+  set state(state: string) {
+      console.log('app1 received state', state);
+  }
+
+  @Output() message = new EventEmitter<any>();
+
+  constructor() {
+  	setTimeout(() => {
+  		this.message.emit('After 5 seconds, something happened in app1');
+  	}, 5000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,10 +9,19 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
+  entryComponents: [AppComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: []
 })
-export class AppModule { }
+export class AppModule {
+
+	constructor(private injector: Injector) {}
+
+	ngDoBootstrap() {
+		const appAsCustomElement = createCustomElement(AppComponent, {injector: this.injector});
+		customElements.define('ng-elements-app', appAsCustomElement);
+	}
+	
+}
